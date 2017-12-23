243af25f5d4074a41d247fece2153cd6f9190472
theopus <theopus5@gmail.com>
2017-12-23 21:42:01 +0200

    additional tests

---
+++ knuca-schedule/knuca-schedule-repository/src/test/java/com/theopus/repository/service/impl/CacheableCourseServiceTest.java
@@ -0,0 +1,151 @@
+package com.theopus.repository.service.impl;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.theopus.entity.schedule.Course;
+import com.theopus.entity.schedule.Subject;
+import com.theopus.entity.schedule.Teacher;
+import com.theopus.entity.schedule.enums.LessonType;
+import com.theopus.repository.jparepo.CourseRepository;
+import com.theopus.repository.jparepo.SubjectRepository;
+import com.theopus.repository.jparepo.TeacherRepository;
+import com.theopus.repository.service.CourseService;
+import com.theopus.repository.specification.TeacherSpecification;
+import conf.DataBaseServiceConfigTest;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Oleksandr_Tkachov on 23.12.2017.
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = DataBaseServiceConfigTest.class)
+public class CacheableCourseServiceTest {
+
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private TeacherRepository teacherRepository;
+    @Autowired
+    private SubjectRepository subjectRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        courseService.flush();
+        courseRepository.deleteAll();
+        teacherRepository.deleteAll();
+        subjectRepository.deleteAll();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        courseService.flush();
+        courseRepository.deleteAll();
+        teacherRepository.deleteAll();
+        subjectRepository.deleteAll();
+    }
+
+    @Test
+    public void sameCourseSave() throws Exception {
+        String subjectName = "test_subject_1";
+        String teacherName_1 = "test_teacher_1";
+        String teacherName_2 = "test_teacher_2";
+        LessonType lessonType = LessonType.LAB;
+        Course course_1 = new Course(new Subject(subjectName), lessonType, Sets.newHashSet(
+                new Teacher(teacherName_1),
+                new Teacher(teacherName_2)
+        ));
+
+        Course course_2 = new Course(new Subject(subjectName), lessonType, Sets.newHashSet(
+                new Teacher(teacherName_1),
+                new Teacher(teacherName_2)
+        ));
+        List<Course> expected = Collections.singletonList(course_1);
+        courseService.save(course_1);
+        courseService.save(course_2);
+        List<Course> actual = (List<Course>) courseService.getAll();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void sameCourseSave_different_teachers() throws Exception {
+        String subjectName = "test_subject_1";
+        String teacherName_1 = "test_teacher_1";
+        String teacherName_2 = "test_teacher_2";
+        LessonType lessonType = LessonType.LAB;
+        Course course_1 = new Course(new Subject(subjectName), lessonType, Sets.newHashSet(
+                new Teacher(teacherName_1),
+                new Teacher(teacherName_2)
+        ));
+
+        Course course_2 = new Course(new Subject(subjectName), lessonType, Sets.newHashSet(
+                new Teacher(teacherName_1)
+        ));
+        List<Course> expected = Lists.newArrayList(course_1, course_2);
+        courseService.save(course_1);
+        courseService.save(course_2);
+        List<Course> actual = (List<Course>) courseService.getAll();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void deleteCourseNotAffect() throws Exception {
+        String subjectName = "test_subject_1";
+        String teacherName_1 = "test_teacher_1";
+        String teacherName_2 = "test_teacher_2";
+        LessonType lessonType = LessonType.LAB;
+        Course course_1 = new Course(new Subject(subjectName), lessonType, Sets.newHashSet(
+                new Teacher(teacherName_1),
+                new Teacher(teacherName_2)
+        ));
+        List<Teacher> expected_teachers = Lists.newArrayList(new Teacher(teacherName_1), new Teacher(teacherName_2));
+        List<Subject> expected_subject = Lists.newArrayList(new Subject(subjectName));
+        courseService.save(course_1);
+        courseRepository.delete(course_1);
+        List<Teacher> actual_teachers = teacherRepository.findAll();
+        List<Subject> actual_subject = subjectRepository.findAll();
+
+        assertEquals(expected_teachers,actual_teachers);
+        assertEquals(expected_subject,actual_subject);
+        assertEquals(courseRepository.count(),0);
+    }
+
+    @Test
+    public void deleteTeacherNotAffect() throws Exception {
+        String subjectName = "test_subject_1";
+        String teacherName_1 = "test_teacher_1";
+        String teacherName_2 = "test_teacher_2";
+        LessonType lessonType = LessonType.LAB;
+        Course course_1 = new Course(new Subject(subjectName), lessonType, Sets.newHashSet(
+                new Teacher(teacherName_1),
+                new Teacher(teacherName_2)
+        ));
+
+        List<Course> expected = Lists.newArrayList(
+                new Course(
+                        new Subject(subjectName), lessonType, Sets.newHashSet(
+                                new Teacher(teacherName_1)
+        )));
+        courseService.save(course_1);
+        System.out.println(teacherRepository.findAll());
+        teacherRepository.delete(teacherRepository.findAll(TeacherSpecification.getByName(teacherName_2)));
+        List<Course> actual = (List<Course>) courseService.getAll();
+
+        assertEquals(expected,actual);
+    }
+}
--- knuca-schedule/knuca-schedule-repository/src/test/java/com/theopus/repository/service/impl/CacheableTeacherServiceTest.java
+++ knuca-schedule/knuca-schedule-repository/src/test/java/com/theopus/repository/service/impl/CacheableTeacherServiceTest.java
@@ -4,6 +4,9 @@ import com.theopus.entity.schedule.Teacher;
 import com.theopus.repository.jparepo.TeacherRepository;
 import com.theopus.repository.service.TeacherService;
 import conf.DataBaseServiceConfigTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +17,34 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ContextConfiguration(classes = DataBaseServiceConfigTest.class)
 public class CacheableTeacherServiceTest {
 
+    @Autowired
+    private TeacherService teacherService;
 
     @Autowired
-    private TeacherRepository repository;
+    private TeacherRepository teacherRepository;
 
+    @After
+    public void tearDown() throws Exception {
+        teacherRepository.deleteAll();
+        teacherService.flush();
+    }
+
+    @Before
+    public void name() throws Exception {
+        teacherRepository.deleteAll();
+        teacherService.flush();
+    }
 
     @Test
     public void saveSameTeacher() throws Exception {
-        TeacherService teacherService = new CacheableTeacherService(repository);
-        teacherService.save(new Teacher("228"));
+        String name = "testname";
+        Long expected = 1L;
+        teacherService.save(new Teacher(name));
+        teacherService.save(new Teacher(name));
+        teacherService.save(new Teacher(name));
+
+        Long actual = teacherService.size();
+
+        Assert.assertEquals(expected,actual);
     }
 }
