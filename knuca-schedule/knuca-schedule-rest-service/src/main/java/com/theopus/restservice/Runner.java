7f693461737f966475f8ae8fcee0b3a13b81b363
theopus <theopus5@gmail.com>
2018-01-02 15:51:53 +0200

    Moved config to repository module.

--- knuca-schedule/knuca-schedule-rest-service/src/main/java/com/theopus/restservice/Runner.java
+++ knuca-schedule/knuca-schedule-rest-service/src/main/java/com/theopus/restservice/Runner.java
@@ -2,8 +2,10 @@ package com.theopus.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.theopus.repository.config")
 public class Runner {
     public static void main(String[] args) {
         SpringApplication.run(Runner.class);
