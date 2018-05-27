6ece7d350cdb576b7c591d41d869889a3b001feb
theopus <theopus5@gmail.com>
2018-05-27 21:04:58 +0300

    Cors commit

--- knuca-schedule/knuca-schedule-rest-service/src/main/java/com/theopus/restservice/RestRunner.java
+++ knuca-schedule/knuca-schedule-rest-service/src/main/java/com/theopus/restservice/RestRunner.java
@@ -2,7 +2,11 @@ package com.theopus.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan("com.theopus.repository.config")
@@ -10,6 +14,19 @@ import org.springframework.context.annotation.ComponentScan;
 public class RestRunner {
     public static void main(String[] args) {
         SpringApplication.run(RestRunner.class);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                        .allowedOrigins("*");
+            }
+        };
     }
 
 }
